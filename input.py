aecb6007fa08b3a4a6f57fb6883c10f1a7c73817
luckybox000 <luckycjc000@gmail.com>
2025-12-03 07:30:47 +0900

    first commit

---
+++ input.py
@@ -0,0 +1,5 @@
+#입력값을 받습니다.
+string =input ("입력>")
+#출력합니다.
+print("자료", string)
+print("자료형", type(string))
---
+++ variable01.py
@@ -0,0 +1,30 @@
+# pi = 3.141592
+# r = 10
+
+# #변수 참조
+# print("원주율=", pi )
+# print("반지름=", r )
+# print("원의 둘레=", 2 * pi * r ) #원의 둘레
+# print("원의 넓이=", pi * r * r ) #원의 넓이
+
+# number=100
+# number += 10
+# number += 20
+# number += 30
+
+
+# print("number :", number)
+
+# string = "Hello coding Python"
+# string = string + "!"
+# string += "!"
+# print("string : ", string)
+# print("string : ", string)
+# print("string : ", string)
+
+string = input("인사말을 입력하세요")
+
+print("입력한 인사말 :", string)
+print("인사말의 길이 :", len(string))
+print("인사말의 자료형 :", type(string))
+
